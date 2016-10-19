d3e78e4b24c1b077e000b3a1ac353ebb1dd2f138
mfidemraizer <fidemraizer@hotmail.com>
2016-10-19 23:02:03 +0200

    Version 0

---
+++ gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+      connect: {
+        server: {
+          options: {
+            port: 8080,
+            base: ".",
+            keepalive: true
+          }
+        }
+      }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+};
---
+++ js/init.js
@@ -0,0 +1,3 @@
+System.import("js/app.js").then(function() {
+    console.log("App started!");
+});
