e5c4c6ade6b2732a3efa6e40aab02db337d1f618
Alumno12K3 <Alumno12K3@AulasKnowtech.es>
2016-10-25 21:05:13 +0200

    prototype

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-debugger;
+import "./oop.js";
+
+function A() {
+  this.x = 11;
+};
+
+A.prototype = {
+    doStuff : function() {
+      console.log("hey");
+    }
+};
+
+var instanceOfA = new A();
+
+//PROTOTYPE CHAIN
+instanceOfA.doStuff();
+
+function B() {};
+B.prototype = Object.create(A.prototype);
+
+var instanceOfB = new B();
+instanceOfB.doStuff();
